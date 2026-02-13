1f4a6e1eccc55b578bf604697447b2fd810c2330
NathimKhan <nathim5757@gmail.com>
2026-02-13 17:43:47 +0530

    Fix asset imports and optimize build

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import windIcon from '../assets/wind.png';
-import humidityIcon from '../assets/humidity.png';
-import visibilityIcon from '../assets/visibility.png';
-import sunriseIcon from '../assets/sunrise.png';
-import sunsetIcon from '../assets/sunset.png';
+import windIcon from '/src/assets/wind.png';
+import humidityIcon from '/src/assets/humidity.png';
+import visibilityIcon from '/src/assets/visibility.png';
+import sunriseIcon from '/src/assets/sunrise.png';
+import sunsetIcon from '/src/assets/sunset.png';
 
 const Icon = ({ src, alt, className }) => (
     <img src={src} alt={alt} className={`h-8 w-8 inline-block ${className}`} />
